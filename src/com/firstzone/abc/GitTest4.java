44a776f6a8a0fe56139f9c06bdb01ad4420bf436
JeoungJin <wed0406@daum.net>
2025-03-06 14:20:08 +0900

    github dev접근 add

---
+++ src/com/firstzone/abc/GitTest4.java
@@ -0,0 +1,8 @@
+package com.firstzone.abc;
+
+public class GitTest3 {
+	public static void main(String[] args) {
+        System.out.println("github dev에서 코드 작성해보기..시작은 .을 누르면 자동연결")
+    }
+     
+}
